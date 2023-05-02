166527d0bc177ea8252a73a4acc58f95d2b10b0c
Jo Wagner <jowagner@limago.de>
2023-05-02 15:57:06 +0200

    Montag Abend

--- NimGameProject/src/de/atlas/game/nim/NimGameImpl.java
+++ NimGameProject/src/de/atlas/game/nim/NimGameImpl.java
@@ -7,16 +7,16 @@ import java.util.Scanner;
 public class NimGameImpl implements Game {
     private Scanner scanner = new Scanner(System.in);
     private int stones;
-    private boolean gameover;
+    private int turn;
 
     public NimGameImpl() {
         stones = 23;
-        gameover = false;
+
     }
 
     @Override
     public void play() {
-        while(! gameover) {
+        while(! isGameover()) {
             playSingleRound();
         }
     }
@@ -27,33 +27,49 @@ public class NimGameImpl implements Game {
     }
 
     private void humanTurn() {
-        int turn;
+        if(isGameover()) return;
+        executeTurn();
+        terminateTurn("Mensch");
+    }
 
+    private void executeTurn() {
         while(true) {
-            System.out.println(String.format("Es gibt %s Steine. Bitte nehmen Sie 1, 2 oder 3!", stones));
+            write(String.format("Es gibt %s Steine. Bitte nehmen Sie 1, 2 oder 3!", stones));
             turn = scanner.nextInt();
             if(turn >= 1 && turn <= 3) break;
-            System.out.println("Ungueltiger Zug.");
+            write("Ungueltiger Zug.");
         }
-        stones -= turn;
     }
 
     private void computerTurn() {
+        if(isGameover()) return;
         final int turns[] = {3,1,1,2};
 
-        if(stones < 1) {
-            System.out.println("Du Loser");
-            gameover = true;
-            return;
-        }
-        if(stones == 1) {
-            System.out.println("Du hast nur Glueck gehabt");
-            gameover = true;
-            return;
-        }
+        turn = turns[stones % 4];
+        write(String.format("Computer nimmt %s Steine.", turn));
+        terminateTurn( "Computer");
+    }
 
-        int turn = turns[stones % 4];
-        System.out.println(String.format("Computer nimmt %s Steine.", turn));
+    private void terminateTurn( String playerName) { // Integration (ruft Unterprogramme auf)
+        updateBoard();
+        printGameOverMessageIfGameIsOver(playerName);
+    }
+
+    private void printGameOverMessageIfGameIsOver(final String playerName) { // Operation
+        if(isGameover()) {
+           write(String.format("%s hat verloren", playerName));
+        }
+    }
+
+    private void write(String message) {
+        System.out.println(message);
+    }
+
+    private void updateBoard() {
         stones -= turn;
+    }
+
+    private boolean isGameover() {
+        return stones < 1;
     }
 }
